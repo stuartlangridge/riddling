502ddb69b4ea02a1fa1e2d5eee74cd00094f7393
sil <sil@kryogenix.org>
2014-02-18 20:57:21 +0000

    Add splashscreen for iOS

--- make_icons.py
+++ make_icons.py
@@ -8,6 +8,27 @@ def main():
     fp.close()
     dom = minidom.parseString(data)
     svg = os.path.join(os.path.dirname(__file__), "resources", "Riddling.svg")
+    
+    for splashel in dom.documentElement.getElementsByTagName("gap:splash"):
+        parts = splashel.getAttribute("src").split("/")
+        fn = os.path.join(os.path.dirname(__file__), "www", *parts)
+        fol = os.path.dirname(fn)
+        width = int(splashel.getAttribute("width"))
+        height = int(splashel.getAttribute("height"))
+        try:
+            os.makedirs(fol)
+        except OSError as e:
+            if e.errno == 17: 
+                pass
+            else:
+                raise
+        newsize = "%sx%s" % (width, height)
+        iconsize = (200,200)
+        geometry = "%sx%s+%s+%s" % (iconsize[0], iconsize[1], (width/2) - (iconsize[0]/2), (height/2) - (iconsize[1]/2))
+        cmd = ["convert", "-size", newsize, "canvas:#ebebeb", svg, "-geometry", geometry, "-composite", fn]
+        print "Creating %sx%s splash as %s" % (width, height, fn)
+        subprocess.call(cmd)
+
     for iconel in dom.documentElement.getElementsByTagName("icon"):
         parts = iconel.getAttribute("src").split("/")
         fn = os.path.join(os.path.dirname(__file__), "www", *parts)
