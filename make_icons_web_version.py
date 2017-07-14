54e3a131501293696bf838d30a64a0a30896a229
Stuart Langridge <sil@kryogenix.org>
2017-07-14 23:14:19 +0100

    Start updating the new web version; add help (which is ugly); add favicons

---
+++ make_icons_web_version.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import os, subprocess
+
+def main():
+    root = os.path.join(os.path.dirname(__file__), "web-version")
+    index = os.path.join(root, "index.html")
+    fp = open(index)
+    html = fp.read()
+    fp.close()
+    soup = BeautifulSoup(html, "lxml")
+    svg = os.path.join(os.path.dirname(__file__), "resources", "Riddling.svg")
+    
+    favicons = soup.select("link[rel=icon]")
+    appleicons = soup.select("link[rel=apple-touch-icon]")
+
+    for icon in favicons + appleicons:
+        fn = os.path.join(root, icon["href"])
+        fol = os.path.dirname(fn)
+        try:
+            os.makedirs(fol)
+        except OSError as e:
+            if e.errno == 17: 
+                pass
+            else:
+                raise
+
+        size = int(os.path.splitext(fn)[0].split("-")[-1])
+
+        newsize = "%sx%s" % (size, size)
+        cmd = ["convert", svg, "-resize", newsize, fn]
+        print "Creating %sx%s icon as %s" % (size, size, fn)
+        subprocess.call(cmd)
+
+if __name__ == "__main__":
+    main()
